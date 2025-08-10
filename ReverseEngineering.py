e3b66b670f6e308d119263f1104921d7efc25883
Korede Anunusor <koredeanunusor@gmail.com>
2025-08-10 20:49:32 +0100

    reverse engineering

--- ReverseEngineering.py
+++ ReverseEngineering.py
@@ -2,15 +2,15 @@
 
 def reverse_number(n):
     reversed_num = 0
-    is_negative = n < 0
+    # is_negative = n < 0
     n = abs(n)
 
     while n > 0:
         digit = n % 10
         reversed_num = reversed_num * 10 + digit
-        n //= 10
+        n = int(n / 10)
 
-    return -reversed_num if is_negative else reversed_num
+    return reversed_num
 
 # Example usage:
 print(reverse_number(1234))  # Output: 4321
